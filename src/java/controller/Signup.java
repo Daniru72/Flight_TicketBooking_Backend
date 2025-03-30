83b3220566c1b88e42499f00ce46469b9972fca3
Daniru72 <Daniru@dev.athenaholdingsllc.com>
2025-03-30 19:12:20 +0530

    signup process

--- src/java/controller/Signup.java
+++ src/java/controller/Signup.java
@@ -1,27 +1,107 @@
-
 package controller;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import entity.Status;
+import entity.User;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import model.HibernateUtil;
+import model.Validation;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
 @WebServlet(name = "Signup", urlPatterns = {"/Signup"})
 public class Signup extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Gson gson = new Gson();
+        JsonObject requestobject = gson.fromJson(req.getReader(), JsonObject.class);
+        JsonObject responseJson = new JsonObject();
+
+        String fname = requestobject.get("fname").getAsString();
+        String lname = requestobject.get("lname").getAsString();
+        String email = requestobject.get("email").getAsString();
+        String password = requestobject.get("password").getAsString();
+        String mobile = requestobject.get("mobile").getAsString();
+
+        if (fname.isEmpty()) {
+            responseJson.addProperty("message", "Fill your First name");
+        } else if (fname.length() < 45) {
+            responseJson.addProperty("message", "Firstname must be lower than 45 caractors");
+        } else if (!fname.matches("[a-zA-Z]+")) {
+            responseJson.addProperty("message", "First name accept only letters");
+            // Validate first name for being non-blank and alphabetic only
+        } else if (lname.isEmpty()) {
+            responseJson.addProperty("message", "Fill your Lirst name");
+        } else if (lname.length() < 45) {
+            responseJson.addProperty("message", "Lastname must be lower than 45 caractores");
+        } else if (!lname.matches("[a-zA-Z]+")) {
+            responseJson.addProperty("message", "First name accept only letters");
+            // Validate first name for being non-blank and alphabetic only
+
+        } else if (email.isEmpty()) {
+            responseJson.addProperty("message", "Fill your Email address");
+        }else if (!Validation.isEmailValid(email)) {
+            responseJson.addProperty("message", "Invalide Email address");
+        }else if (mobile.isEmpty()) {
+            responseJson.addProperty("message", "Fill your Mobile number");
+        } else if (!mobile.matches("^07[1,2,4,5,6,7,8,0][0-9]{7}$")) {
+            responseJson.addProperty("message", "Invalide Mobile Number");
+        }else if (password.isEmpty()) {
+            responseJson.addProperty("message", "Fill your password");
+        } else if (password.length() >= 5) {
+            responseJson.addProperty("message", "password must be grater than 5 caractors");
+        } else {
+
+            try {
+
+                Session session = HibernateUtil.getSessionFactory().openSession();
+
+                Criteria criteria1 = session.createCriteria(Signup.class);
+                criteria1.add(Restrictions.eq("email", email));
+
+                if (!criteria1.list().isEmpty()) {
+                    //User already Registered
+                    responseJson.addProperty("message", "User already registerd");
+                } else {
+                    //New user
+
+                    User user = new User();
+                    user.setEmail(email);
+                    user.setFname(fname);
+                    user.setLname(lname);
+                    user.setMobile(mobile);
+                    user.setPassword(password);
+                    user.setJoined_date(new Date());
+
+                    Status status = (Status) session.get(Status.class, 1);
+                    user.setUser_Status(status);
+
+                }
+                
+                 session.close();
+
+            } catch (Exception e) {
+                responseJson.addProperty("message", "Server side Error");
+            }
+
+        }
         
-        
-        
-        
+       
+         
+           resp.setContentType("application/json");
+         resp.getWriter().write(gson.toJson(responseJson));
+
     }
 
-    
-    
-    
 }
---
+++ src/java/model/Validation.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author danir
+ */
+public class Validation {
+    
+     public static boolean isEmailValid(String email){
+       return email.matches("^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$");
+    }
+    
+}
